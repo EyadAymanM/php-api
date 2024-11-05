b817cb8309cfd361148874967062542d5e8bda46
EyadAymanM <111561121+EyadAymanM@users.noreply.github.com>
2024-11-05 14:20:05 +0200

    fixing cors

--- api/index.php
+++ api/index.php
@@ -4,9 +4,11 @@ require __DIR__ . '/src/Gateway.php';
 require __DIR__ . '/src/Database.php';
 
 header("Content-type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: https://vocal-pony-a43e3a.netlify.app");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
   http_response_code(200);
