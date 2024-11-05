a75d3aa724fc16880dbb8f9cd8f94aa79605b324
EyadAymanM <111561121+EyadAymanM@users.noreply.github.com>
2024-11-05 14:22:15 +0200

    removing on that may cause conflict

--- api/index.php
+++ api/index.php
@@ -7,7 +7,6 @@ header("Content-type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: https://vocal-pony-a43e3a.netlify.app");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
